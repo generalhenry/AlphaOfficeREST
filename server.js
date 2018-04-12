79f3a36c18e00c725db0451b548460dd1baf22ac
Henry Allen-Tilford <htilford@gmail.com>
2018-04-11 19:06:00 -0700

    clean up
    - Added eslint and `npm run lint` command
    - fixed lint issues (semis, indents )
    - added valid html head
    - made /products into a link
    - removed unused packages from package.json
    - fixed indentation in package.json
    - removed unused requires
    - switched from `var` to `const`
    - renamed myJSON to productCatalog for clairity
    - removed `express.static` in favor of `res.sendFile` for just the index.html (no need to serve up /server.js)
    - switched from `res.end(JSON.stringify` to res.json for brevity and proper headers

--- server.js
+++ server.js
@@ -1,71 +1,56 @@
 'use strict';
 
-var http = require('http')
-var express = require('express')
-var bodyParser = require('body-parser')
-var hostString = process.argv[2]
-var myJSON = require('./product-catalog.json');
+const express = require('express');
+const productCatalog = require('./product-catalog');
 
-
-var port = process.env.PORT || 8002
-var app = express()
-
-
-app.use(bodyParser.json());
-app.use(express.static(__dirname));
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+const port = process.env.PORT || 8002;
+const app = express();
 
 app.get('/', function(req, res) {
-	res.send('index.html')
-})
+  res.sendFile('./index.html', { root: __dirname });
+});
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 /*
  * route /products has been replaced in this example with static JSON data
  */
 
 app.get('/products', function(req, res) {
-
-	console.log('/products api called')
-
-    res.end(JSON.stringify(myJSON));
+  console.log('/products api called');
+  res.json(productCatalog);
 });
 
 app.get('/searchProductNames', function(req, res) {
-
-	console.log('/productNames api called')
-
-  res.end(JSON.stringify(
-    myJSON.Products
-    .filter(product => product.PRODUCT_NAME.includes(req.query.term))
-    .map(
-      product => (({PRODUCT_NAME, PRODUCT_ID}) => ({PRODUCT_NAME, PRODUCT_ID}))(product)
-    )));
-
+  console.log('/productNames api called');
+  res.json(
+    productCatalog.Products
+      .filter(product => product.PRODUCT_NAME.includes(req.query.term))
+      .map(
+        product => (({PRODUCT_NAME, PRODUCT_ID}) => ({PRODUCT_NAME, PRODUCT_ID}))(product)
+      ));
 });
 
 app.get('/productNames', function(req, res) {
-
-	console.log('/productNames api called');
-  res.end(JSON.stringify(
-    myJSON.Products.map(
+  console.log('/productNames api called');
+  res.json(
+    productCatalog.Products.map(
       product => (({PRODUCT_NAME, PRODUCT_ID}) => ({PRODUCT_NAME, PRODUCT_ID}))(product)
-    )));
+    ));
 });
 
 app.get('/product/:id', function(req, res) {
-
-	console.log('/product api called')
-  res.end(JSON.stringify(
-    myJSON.Products.filter(product => product.PRODUCT_ID == req.params.id)
-  ));
+  console.log('/product api called');
+  res.json(
+    productCatalog.Products.filter(product =>
+      product.PRODUCT_ID == req.params.id
+    ));
 });
 
 app.listen(port, function() {
-	console.log('AlphaOffice listening on port ' + port)
-
+  console.log('AlphaOffice listening on port', port);
 });
